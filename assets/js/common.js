d499d7ccd608b823c63ba1e95fa15376b5926405
vikto132 <60517065+vikto132@users.noreply.github.com>
2021-01-25 15:13:26 +0700

    logic-finish

---
+++ assets/js/common.js
@@ -0,0 +1,58 @@
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+function cleanSpaceAndUnicode(str) {
+    return str.replace(/[\s]+/g, '')
+        .replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, 'a')
+        .replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, 'e')
+        .replace(/ì|í|ị|ỉ|ĩ/g, 'i')
+        .replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, 'o')
+        .replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, 'u')
+        .replace(/ỳ|ý|ỵ|ỷ|ỹ/g, 'y')
+        .replace(/đ/g, 'd')
+        .replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, 'A')
+        .replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, 'E')
+        .replace(/Ì|Í|Ị|Ỉ|Ĩ/g, 'I')
+        .replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, 'O')
+        .replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, 'U')
+        .replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, 'Y')
+        .replace(/Đ/g, 'D');
+}
+
+function cleanSpace(str) {
+    return str.replace(/[\s]+/g, '');
+}
+
+function clean(str) {
+    return str.replace(/[!@#$%^&*()_+\-=`~\/\*\-\.,\/;'[\]\{\}\|":<>?\\]{1}/g, '');
+}
+
+function nameFormat(str) {
+    return str.replace(/[0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{1,}/g, '');
+}
+
+function cleanNumber(str) {
+    return str.replace(/[0-9]/g, '');
+}
+
+function getNumber(str) {
+    return str.replace(/[^0-9+]/g, '');
+
+}
